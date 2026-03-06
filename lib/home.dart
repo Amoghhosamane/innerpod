3a3db8b22e9b720ae05b695f11dece5412341fd4
Amoghhosamane <amoghvarsh9614@gmail.com>
2026-03-07 00:36:42 +0530

    fix: Solid Login on Android is failing #68
    Added INTERNET permission to AndroidManifest.xml and updated Home widget to use SolidScaffold.

--- lib/home.dart
+++ lib/home.dart
@@ -140,7 +140,7 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // final dateStr = DateFormat('dd MMMM yyyy').format(DateTime.now());
 
-    return Scaffold(
+    return SolidScaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Row(
